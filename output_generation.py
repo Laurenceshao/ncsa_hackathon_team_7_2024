3995acdc6472a8e7de185f6710d8da804e127018
ethansirois <esirois1193@gmail.com>
2024-04-20 15:04:43 -0500

    updated output

--- output_generation.py
+++ output_generation.py
@@ -5,9 +5,21 @@ path_to_json_file = "step_summary.json" # we will just update this file, no need
 
 def generate_output(executor_output: (str, str)) -> str:
     f = open(path_to_json_file)
-    step_summary_list_json = json.load(f)
+    step_summary_list = json.load(f)
+    new_id = 10
+    new_step_number = 10
+    new_step_summary = {
+        "id": new_id, 
+        "step number": new_step_number,
+        "step summary": executor_output[0],
+        "output of step": executor_output[1]
+    }
+    step_summary_list["step summary list"].append(new_step_summary)
+    with open('step_summary.json', 'w') as f:
+        json.dump(step_summary_list, f)
+    
     summary_of_steps = ""
-    for step_summary in step_summary_list_json["step summary list"]:
+    for step_summary in step_summary_list["step summary list"]:
         summary_of_steps += "Then, " + step_summary["step summary"] + "\n"
 
     return summary_of_steps
